c897abedd35005076e5b78e58f24ee21023073be
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:53:40 +0200

    Kreiraj klasu Kontrola
    Kreiran je kostur klase kontrola koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/Kontrola.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Kontrola
+    {
+        private DateTime datumKontrole;
+        private string opis;
+
+
+
+        public Veterinar Veterinar
+        {
+            get => default(Veterinar);
+            set
+            {
+            }
+        }
+
+        public Zivotinja Zivotinja
+        {
+            get => default(Zivotinja);
+            set
+            {
+            }
+        }
+
+        public StatusAktivnosti StatusAktivnosti
+        {
+            get => default(StatusAktivnosti);
+            set
+            {
+            }
+        }
+    }
+}
