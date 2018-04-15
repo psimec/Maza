c0bf217f78877c2ed3da9b650fe37bf980460ec5
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:52:24 +0200

    Kreiraj klasu CijepivoZaZivotinju
    Kreiran je kostur klase cijepivo za zivotinju koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/CijepivoZaZivotinju.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class CijepivoZaZivotinju
+    {
+        private DateTime datum;
+
+        public Kontrola Kontrola
+        {
+            get => default(Kontrola);
+            set
+            {
+            }
+        }
+
+        public Cijepivo Cijepivo
+        {
+            get => default(Cijepivo);
+            set
+            {
+            }
+        }
+    }
+}
