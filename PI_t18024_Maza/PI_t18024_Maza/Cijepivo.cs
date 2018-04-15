d34c7492c48321c257cd55295add5b525483f49d
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:51:43 +0200

    Kreiraj klasu Cijepivo
    Kreiran je kostur klase cijepivo koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/Cijepivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Cijepivo
+    {
+        private string imeCijepiva;
+        private string doza;
+        private string proizvodac;
+        private DateTime rokTrajanja;
+        private float cijena;
+    }
+}
