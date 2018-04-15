3216dbdaeeeef3b61247c0f801fc37c93e769ad2
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:57:51 +0200

    Kreiraj enumeraciju StatusAktivnosti
    Kreirana je enumerijacija koja označava da li je aktivnost obavljena ili ne

---
+++ PI_t18024_Maza/PI_t18024_Maza/StatusAktivnosti.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public enum StatusAktivnosti
+    {
+        obavljena,
+        neObavljena
+    }
+}
