3c4bb87ef9d908bac3385c0d0ea69cd519bcf49b
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:54:31 +0200

    Kreiraj klasu Operacija
    Kreiran je kostur klase operacija koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/Operacija.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Operacija
+    {
+        private string vrstaZahvata;
+        private int trajanjeZahvata;
+        private string napomena;
+
+        public Dijagnoza Dijagnoza
+        {
+            get => default(Dijagnoza);
+            set
+            {
+            }
+        }
+    }
+}
