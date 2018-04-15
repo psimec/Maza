46de3abb6eba838f01970dd5eab0bed70a594890
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:56:46 +0200

    Kreiraj klasu VrstaZivotinja
    Kreiran je kostur klase vrsta zivotinje koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/VrstaZivotinje.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class VrstaZivotinje : Zivotinja
+    {
+        private string naziv;
+        private string pasmina;
+    }
+}
