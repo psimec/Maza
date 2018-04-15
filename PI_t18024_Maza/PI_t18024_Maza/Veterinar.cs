09157129e167d228a6d36d41ecf08b25797609f3
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:55:36 +0200

    Kreiraj klasu Veterinar
    Kreiran je kostur klase veterinar koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/Veterinar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Veterinar
+    {
+        private string ime;
+        private string prezime;
+        private string adresaStanovanja;
+        private string brojTelefona1;
+        private string brojTelefona2;
+        private string stupanjObrazovanja;
+        private DateTime datumZaposljavanja;
+        private DateTime datumRodenja;
+    }
+}
