ff1916b75add098f921ffc979b7521a1dd5a81c0
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:52:53 +0200

    Kreiraj klasu Dijagnoza
    Kreiran je kostur klase dijagnoza koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/Dijagnoza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Dijagnoza
+    {
+        private string simptomi;
+        private string nazivBolesti;
+        private string terapija;
+        private string napomena;
+
+        public Kontrola Kontrola
+        {
+            get => default(Kontrola);
+            set
+            {
+            }
+        }
+    }
+}
