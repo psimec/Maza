4ce601c98ba0d3df1469e85558fa07a1945a139b
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:56:19 +0200

    Kreiraj klasu Zivotinja
    Kreiran je kostur klase zivotinja koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/Zivotinja.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Zivotinja
+    {
+        private string ime;
+        private char spol;
+        private DateTime datumRodenja;
+        private int brojCipa;
+        private int tezina;
+
+        public Vlasnik Vlasnik
+        {
+            get => default(Vlasnik);
+            set
+            {
+            }
+        }
+    }
+}
