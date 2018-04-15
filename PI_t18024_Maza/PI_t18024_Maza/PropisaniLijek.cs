9d17ed9062a9f0ff6bc3f4b92eaa1e30cfc9e88c
Paskal Šimec <psimec@foi.hr>
2018-04-15 15:55:12 +0200

    Kreiraj klasu PropisaniLijek
    Kreiran je kostur klase propisani lijek koji sarži samo atribute klase

---
+++ PI_t18024_Maza/PI_t18024_Maza/PropisaniLijek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class PropisaniLijek
+    {
+        private string kolicina;
+
+        public Dijagnoza Dijagnoza
+        {
+            get => default(Dijagnoza);
+            set
+            {
+            }
+        }
+
+        public Lijek Lijek
+        {
+            get => default(Lijek);
+            set
+            {
+            }
+        }
+    }
+}
