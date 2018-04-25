ebe27135f5b5f84816a5a27b7a93c8a89b203d05
Paskal Šimec <psimec@foi.hr>
2018-04-25 16:56:59 +0200

    Kreiraj klasu Pas
    Kreirana je klasa Pas

---
+++ PI_t18024_Maza/PI_t18024_Maza/Pas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_t18024_Maza
+{
+    public class Pas : Zivotinja
+    {
+        private string brojCipa;
+    }
+}
