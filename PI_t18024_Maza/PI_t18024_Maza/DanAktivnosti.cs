8ed23ea50eb0729567deb0ebf9340377f7d7b0ea
Paskal Šimec <psimec@foi.hr>
2018-04-27 10:52:56 +0200

    Kreiraj klasu DanAktinosti
    Kreirana je klasa DAnAktivnosti koja sadrži podatke vezane za jedan dan u kalendaru

---
+++ PI_t18024_Maza/PI_t18024_Maza/DanAktivnosti.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_t18024_Maza
+{
+    public class DanAktivnosti
+    {
+        public DayOfWeek Dan;
+        public static int X;
+        public int Y;
+        public int BrojAktivnosti;
+
+        public DanAktivnosti(DayOfWeek dan, int Y, int brojAktivnosti)
+        {
+            this.Dan = dan;
+            this.Y = Y;
+            this.BrojAktivnosti = brojAktivnosti;
+        }
+    }
+}
