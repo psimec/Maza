7832bba6cbc1be97636e4131eb2afd44aabd428e
Paskal Šimec <psimec@foi.hr>
2018-04-28 00:16:37 +0200

    Ažuriraj klasu DanAktivnosti
    Obisani su bespotrebne varijable iz klase DanAktivnosti

--- PI_t18024_Maza/PI_t18024_Maza/DanAktivnosti.cs
+++ PI_t18024_Maza/PI_t18024_Maza/DanAktivnosti.cs
@@ -9,14 +9,11 @@ namespace PI_t18024_Maza
     public class DanAktivnosti
     {
         public DayOfWeek Dan;
-        public static int X;
-        public int Y;
         public int BrojAktivnosti;
 
-        public DanAktivnosti(DayOfWeek dan, int Y, int brojAktivnosti)
+        public DanAktivnosti(DayOfWeek dan, int brojAktivnosti)
         {
             this.Dan = dan;
-            this.Y = Y;
             this.BrojAktivnosti = brojAktivnosti;
         }
     }
