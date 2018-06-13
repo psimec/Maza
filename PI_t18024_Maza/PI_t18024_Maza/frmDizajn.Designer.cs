327666b3ee0afbb857820b512b0d21709b0472f2
Paskal Šimec <psimec@foi.hr>
2018-06-13 13:39:43 +0200

    Promjeni logo
    Promijenjen je logo

--- PI_t18024_Maza/PI_t18024_Maza/frmDizajn.Designer.cs
+++ PI_t18024_Maza/PI_t18024_Maza/frmDizajn.Designer.cs
@@ -36,20 +36,19 @@
             this.uiActionZivotinje = new System.Windows.Forms.Button();
             this.uiActionKalendar = new System.Windows.Forms.Button();
             this.LogoPanel = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.Maza = new System.Windows.Forms.Label();
             this.ZaglavljePanel = new System.Windows.Forms.Panel();
             this.uiActionMinimize = new System.Windows.Forms.Button();
             this.uiActionClose = new System.Windows.Forms.Button();
             this.vlasnikBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this._18024_DBDataSet = new PI_t18024_Maza._18024_DBDataSet();
             this.vlasnikTableAdapter = new PI_t18024_Maza._18024_DBDataSetTableAdapters.vlasnikTableAdapter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.LijeviPanel.SuspendLayout();
             this.LogoPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.ZaglavljePanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.vlasnikBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._18024_DBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // LijeviPanel
@@ -61,8 +60,9 @@
             this.LijeviPanel.Controls.Add(this.LogoPanel);
             this.LijeviPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.LijeviPanel.Location = new System.Drawing.Point(0, 0);
+            this.LijeviPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LijeviPanel.Name = "LijeviPanel";
-            this.LijeviPanel.Size = new System.Drawing.Size(216, 778);
+            this.LijeviPanel.Size = new System.Drawing.Size(162, 632);
             this.LijeviPanel.TabIndex = 0;
             // 
             // uiActionKontrola
@@ -71,9 +71,10 @@
             this.uiActionKontrola.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uiActionKontrola.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.uiActionKontrola.Image = ((System.Drawing.Image)(resources.GetObject("uiActionKontrola.Image")));
-            this.uiActionKontrola.Location = new System.Drawing.Point(1, 424);
+            this.uiActionKontrola.Location = new System.Drawing.Point(1, 344);
+            this.uiActionKontrola.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.uiActionKontrola.Name = "uiActionKontrola";
-            this.uiActionKontrola.Size = new System.Drawing.Size(212, 100);
+            this.uiActionKontrola.Size = new System.Drawing.Size(159, 81);
             this.uiActionKontrola.TabIndex = 3;
             this.uiActionKontrola.Text = "Kontrola";
             this.uiActionKontrola.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -86,9 +87,10 @@
             this.uiActionVlasnici.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uiActionVlasnici.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.uiActionVlasnici.Image = ((System.Drawing.Image)(resources.GetObject("uiActionVlasnici.Image")));
-            this.uiActionVlasnici.Location = new System.Drawing.Point(1, 318);
+            this.uiActionVlasnici.Location = new System.Drawing.Point(1, 258);
+            this.uiActionVlasnici.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.uiActionVlasnici.Name = "uiActionVlasnici";
-            this.uiActionVlasnici.Size = new System.Drawing.Size(212, 100);
+            this.uiActionVlasnici.Size = new System.Drawing.Size(159, 81);
             this.uiActionVlasnici.TabIndex = 3;
             this.uiActionVlasnici.Text = "Vlasnici";
             this.uiActionVlasnici.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -101,9 +103,10 @@
             this.uiActionZivotinje.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uiActionZivotinje.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.uiActionZivotinje.Image = ((System.Drawing.Image)(resources.GetObject("uiActionZivotinje.Image")));
-            this.uiActionZivotinje.Location = new System.Drawing.Point(1, 212);
+            this.uiActionZivotinje.Location = new System.Drawing.Point(1, 172);
+            this.uiActionZivotinje.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.uiActionZivotinje.Name = "uiActionZivotinje";
-            this.uiActionZivotinje.Size = new System.Drawing.Size(212, 100);
+            this.uiActionZivotinje.Size = new System.Drawing.Size(159, 81);
             this.uiActionZivotinje.TabIndex = 3;
             this.uiActionZivotinje.Text = "Životinje";
             this.uiActionZivotinje.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -116,9 +119,10 @@
             this.uiActionKalendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uiActionKalendar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.uiActionKalendar.Image = ((System.Drawing.Image)(resources.GetObject("uiActionKalendar.Image")));
-            this.uiActionKalendar.Location = new System.Drawing.Point(1, 106);
+            this.uiActionKalendar.Location = new System.Drawing.Point(1, 86);
+            this.uiActionKalendar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.uiActionKalendar.Name = "uiActionKalendar";
-            this.uiActionKalendar.Size = new System.Drawing.Size(212, 100);
+            this.uiActionKalendar.Size = new System.Drawing.Size(159, 81);
             this.uiActionKalendar.TabIndex = 3;
             this.uiActionKalendar.Text = "Kalendar";
             this.uiActionKalendar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -129,32 +133,12 @@
             // 
             this.LogoPanel.BackColor = System.Drawing.Color.White;
             this.LogoPanel.Controls.Add(this.pictureBox1);
-            this.LogoPanel.Controls.Add(this.Maza);
             this.LogoPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.LogoPanel.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LogoPanel.Name = "LogoPanel";
-            this.LogoPanel.Size = new System.Drawing.Size(216, 100);
+            this.LogoPanel.Size = new System.Drawing.Size(162, 81);
             this.LogoPanel.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Maza
-            // 
-            this.Maza.AutoSize = true;
-            this.Maza.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Maza.Location = new System.Drawing.Point(83, 29);
-            this.Maza.Name = "Maza";
-            this.Maza.Size = new System.Drawing.Size(96, 34);
-            this.Maza.TabIndex = 0;
-            this.Maza.Text = "MAZA";
             // 
             // ZaglavljePanel
             // 
@@ -162,9 +146,10 @@
             this.ZaglavljePanel.Controls.Add(this.uiActionMinimize);
             this.ZaglavljePanel.Controls.Add(this.uiActionClose);
             this.ZaglavljePanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.ZaglavljePanel.Location = new System.Drawing.Point(216, 0);
+            this.ZaglavljePanel.Location = new System.Drawing.Point(162, 0);
+            this.ZaglavljePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ZaglavljePanel.Name = "ZaglavljePanel";
-            this.ZaglavljePanel.Size = new System.Drawing.Size(1128, 100);
+            this.ZaglavljePanel.Size = new System.Drawing.Size(846, 81);
             this.ZaglavljePanel.TabIndex = 1;
             // 
             // uiActionMinimize
@@ -172,10 +157,10 @@
             this.uiActionMinimize.FlatAppearance.BorderSize = 0;
             this.uiActionMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uiActionMinimize.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uiActionMinimize.Location = new System.Drawing.Point(1049, 3);
-            this.uiActionMinimize.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.uiActionMinimize.Location = new System.Drawing.Point(787, 2);
+            this.uiActionMinimize.Margin = new System.Windows.Forms.Padding(2, 8, 8, 2);
             this.uiActionMinimize.Name = "uiActionMinimize";
-            this.uiActionMinimize.Size = new System.Drawing.Size(35, 48);
+            this.uiActionMinimize.Size = new System.Drawing.Size(26, 39);
             this.uiActionMinimize.TabIndex = 0;
             this.uiActionMinimize.Text = "_";
             this.uiActionMinimize.UseVisualStyleBackColor = true;
@@ -186,10 +171,10 @@
             this.uiActionClose.FlatAppearance.BorderSize = 0;
             this.uiActionClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.uiActionClose.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uiActionClose.Location = new System.Drawing.Point(1090, 3);
-            this.uiActionClose.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.uiActionClose.Location = new System.Drawing.Point(818, 2);
+            this.uiActionClose.Margin = new System.Windows.Forms.Padding(2, 8, 8, 2);
             this.uiActionClose.Name = "uiActionClose";
-            this.uiActionClose.Size = new System.Drawing.Size(35, 48);
+            this.uiActionClose.Size = new System.Drawing.Size(26, 39);
             this.uiActionClose.TabIndex = 0;
             this.uiActionClose.Text = "X";
             this.uiActionClose.UseVisualStyleBackColor = true;
@@ -209,25 +194,35 @@
             // 
             this.vlasnikTableAdapter.ClearBeforeFill = true;
             // 
-            // Dizajn
+            // pictureBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmDizajn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(212)))), ((int)(((byte)(249)))));
-            this.ClientSize = new System.Drawing.Size(1344, 778);
+            this.ClientSize = new System.Drawing.Size(1008, 632);
             this.Controls.Add(this.ZaglavljePanel);
             this.Controls.Add(this.LijeviPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Dizajn";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmDizajn";
             this.Text = "Dizajn";
             this.LijeviPanel.ResumeLayout(false);
             this.LogoPanel.ResumeLayout(false);
-            this.LogoPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ZaglavljePanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.vlasnikBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this._18024_DBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -236,8 +231,6 @@
 
         private System.Windows.Forms.Panel LijeviPanel;
         private System.Windows.Forms.Panel LogoPanel;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label Maza;
         private System.Windows.Forms.Panel ZaglavljePanel;
         private System.Windows.Forms.Button uiActionKalendar;
         private System.Windows.Forms.Button uiActionKontrola;
@@ -248,5 +241,6 @@
         private _18024_DBDataSet _18024_DBDataSet;
         private System.Windows.Forms.BindingSource vlasnikBindingSource;
         private _18024_DBDataSetTableAdapters.vlasnikTableAdapter vlasnikTableAdapter;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
