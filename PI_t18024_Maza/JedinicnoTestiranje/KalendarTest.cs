28ec7268128c9994f2076747b5216f28d34a5ece
Paskal Šimec <psimec@foi.hr>
2018-06-19 17:01:06 +0200

    Kreraj projekt za jedinicno testiranje
    Kreiran je projekt za jedinicno testiranje

---
+++ PI_t18024_Maza/JedinicnoTestiranje/KalendarTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PI_t18024_Maza;
+
+namespace JedinicnoTestiranje
+{
+    [TestClass]
+    public class KalendarTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PI_t18024_Maza/JedinicnoTestiranje/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JedinicnoTestiranje")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JedinicnoTestiranje")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5cfeebd-1ba6-4ae4-ad23-47b22c5893c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
