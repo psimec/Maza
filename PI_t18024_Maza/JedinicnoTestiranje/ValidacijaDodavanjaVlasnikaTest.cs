72e6664430b2d72384c89c9015080e4f3ff46feb
Tomislav Šimović <tsimovic@foi.hr>
2018-06-19 17:49:14 +0200

    Izradi testiranje za validaciju unosa vlasnika
    Izrađeno testiranje za validaciju unosa vlasnika

---
+++ PI_t18024_Maza/JedinicnoTestiranje/ValidacijaDodavanjaVlasnikaTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PI_t18024_Maza;
+
+namespace JedinicnoTestiranje
+{
+    [TestClass]
+    public class ValidacijaDodavanjaVlasnikaTest
+    {
+        [TestMethod]
+        public void ValidacijaUnosa1()
+        {
+            PI_t18024_Maza.frmAzurirajVlasnike azurirajVlasnike1 = new frmAzurirajVlasnike();
+            bool validacija1 = azurirajVlasnike1.ValidacijaVlasnika("Marko", "Markić","Ulica 1232");
+            Assert.AreEqual(true, validacija1, "Unos je ispravan");
+        }
+        [TestMethod]
+        public void ValidacijaUnosa2()
+        {
+            PI_t18024_Maza.frmAzurirajVlasnike azurirajVlasnike1 = new frmAzurirajVlasnike();
+            bool validacija2 = azurirajVlasnike1.ValidacijaVlasnika("123", "Markić", "Ulica 1232");
+            Assert.AreEqual(false, validacija2, "Unos je ispravan");
+        }
+    }
+}
