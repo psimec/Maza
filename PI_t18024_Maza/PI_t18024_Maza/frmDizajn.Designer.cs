44f7f6f0454c79396796f8cdf75e678d91bf5955
Paskal Šimec <psimec@foi.hr>
2018-06-19 16:45:29 +0200

    Uredi izgleda buttona na frmKontrola
    Ureden je izgled button na frmKontrola

--- PI_t18024_Maza/PI_t18024_Maza/frmDizajn.Designer.cs
+++ PI_t18024_Maza/PI_t18024_Maza/frmDizajn.Designer.cs
@@ -76,7 +76,6 @@
             this.button1.Text = "Statistika";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // uiActionKontrola
             // 
