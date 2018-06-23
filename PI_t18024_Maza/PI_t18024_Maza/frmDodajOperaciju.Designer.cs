4eeab4d4d959ffc299401974e06007fe2b92965c
Dario Šiprak <dario.siprak@gmail.com>
2018-06-23 10:43:32 +0200

    Kreiraj izgled forme frmDodajOperaciju
    Kreiran je početni izgled forme za dodavanje nove operacije

--- PI_t18024_Maza/PI_t18024_Maza/frmDodajOperaciju.Designer.cs
+++ PI_t18024_Maza/PI_t18024_Maza/frmDodajOperaciju.Designer.cs
@@ -28,19 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.uiPodaciOZivotinji = new System.Windows.Forms.GroupBox();
+            this.uiDatumRodenjaZivotinje = new System.Windows.Forms.Label();
+            this.uiVrstaZivotinje = new System.Windows.Forms.Label();
+            this.uiImeZivotinje = new System.Windows.Forms.Label();
+            this.uiVlasnikZivotinje = new System.Windows.Forms.Label();
+            this.uiDatumZahvata = new System.Windows.Forms.Label();
+            this.uiDatumZahvataTekst = new System.Windows.Forms.Label();
+            this.uiVrstaZahvata = new System.Windows.Forms.Label();
+            this.uiTrajanjeZahvata = new System.Windows.Forms.Label();
+            this.uiNapomena = new System.Windows.Forms.Label();
+            this.uiVrstaZahvataUnos = new System.Windows.Forms.TextBox();
+            this.uiNapomenaUnos = new System.Windows.Forms.TextBox();
+            this.uiTrajanjeZahvataUnos = new System.Windows.Forms.DateTimePicker();
+            this.uiActionDodajOperaciju = new System.Windows.Forms.Button();
+            this.uiActionOdustani = new System.Windows.Forms.Button();
+            this.uiPodaciOZivotinji.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // uiPodaciOZivotinji
+            // 
+            this.uiPodaciOZivotinji.Controls.Add(this.uiDatumRodenjaZivotinje);
+            this.uiPodaciOZivotinji.Controls.Add(this.uiVrstaZivotinje);
+            this.uiPodaciOZivotinji.Controls.Add(this.uiImeZivotinje);
+            this.uiPodaciOZivotinji.Controls.Add(this.uiVlasnikZivotinje);
+            this.uiPodaciOZivotinji.Location = new System.Drawing.Point(16, 12);
+            this.uiPodaciOZivotinji.Name = "uiPodaciOZivotinji";
+            this.uiPodaciOZivotinji.Size = new System.Drawing.Size(223, 177);
+            this.uiPodaciOZivotinji.TabIndex = 2;
+            this.uiPodaciOZivotinji.TabStop = false;
+            this.uiPodaciOZivotinji.Text = "Podaci o životinji";
+            // 
+            // uiDatumRodenjaZivotinje
+            // 
+            this.uiDatumRodenjaZivotinje.AutoSize = true;
+            this.uiDatumRodenjaZivotinje.Location = new System.Drawing.Point(26, 148);
+            this.uiDatumRodenjaZivotinje.Name = "uiDatumRodenjaZivotinje";
+            this.uiDatumRodenjaZivotinje.Size = new System.Drawing.Size(83, 13);
+            this.uiDatumRodenjaZivotinje.TabIndex = 3;
+            this.uiDatumRodenjaZivotinje.Text = "Datum rođenja: ";
+            // 
+            // uiVrstaZivotinje
+            // 
+            this.uiVrstaZivotinje.AutoSize = true;
+            this.uiVrstaZivotinje.Location = new System.Drawing.Point(25, 109);
+            this.uiVrstaZivotinje.Name = "uiVrstaZivotinje";
+            this.uiVrstaZivotinje.Size = new System.Drawing.Size(37, 13);
+            this.uiVrstaZivotinje.TabIndex = 2;
+            this.uiVrstaZivotinje.Text = "Vrsta: ";
+            // 
+            // uiImeZivotinje
+            // 
+            this.uiImeZivotinje.AutoSize = true;
+            this.uiImeZivotinje.Location = new System.Drawing.Point(24, 71);
+            this.uiImeZivotinje.Name = "uiImeZivotinje";
+            this.uiImeZivotinje.Size = new System.Drawing.Size(30, 13);
+            this.uiImeZivotinje.TabIndex = 1;
+            this.uiImeZivotinje.Text = "Ime: ";
+            // 
+            // uiVlasnikZivotinje
+            // 
+            this.uiVlasnikZivotinje.AutoSize = true;
+            this.uiVlasnikZivotinje.Location = new System.Drawing.Point(24, 35);
+            this.uiVlasnikZivotinje.Name = "uiVlasnikZivotinje";
+            this.uiVlasnikZivotinje.Size = new System.Drawing.Size(47, 13);
+            this.uiVlasnikZivotinje.TabIndex = 0;
+            this.uiVlasnikZivotinje.Text = "Vlasnik: ";
+            // 
+            // uiDatumZahvata
+            // 
+            this.uiDatumZahvata.AutoSize = true;
+            this.uiDatumZahvata.Location = new System.Drawing.Point(31, 237);
+            this.uiDatumZahvata.Name = "uiDatumZahvata";
+            this.uiDatumZahvata.Size = new System.Drawing.Size(85, 13);
+            this.uiDatumZahvata.TabIndex = 3;
+            this.uiDatumZahvata.Text = "Datum zahvata: ";
+            // 
+            // uiDatumZahvataTekst
+            // 
+            this.uiDatumZahvataTekst.AutoSize = true;
+            this.uiDatumZahvataTekst.Location = new System.Drawing.Point(121, 237);
+            this.uiDatumZahvataTekst.Name = "uiDatumZahvataTekst";
+            this.uiDatumZahvataTekst.Size = new System.Drawing.Size(35, 13);
+            this.uiDatumZahvataTekst.TabIndex = 4;
+            this.uiDatumZahvataTekst.Text = "label1";
+            // 
+            // uiVrstaZahvata
+            // 
+            this.uiVrstaZahvata.AutoSize = true;
+            this.uiVrstaZahvata.Location = new System.Drawing.Point(32, 271);
+            this.uiVrstaZahvata.Name = "uiVrstaZahvata";
+            this.uiVrstaZahvata.Size = new System.Drawing.Size(78, 13);
+            this.uiVrstaZahvata.TabIndex = 5;
+            this.uiVrstaZahvata.Text = "Vrsta zahvata: ";
+            // 
+            // uiTrajanjeZahvata
+            // 
+            this.uiTrajanjeZahvata.AutoSize = true;
+            this.uiTrajanjeZahvata.Location = new System.Drawing.Point(31, 301);
+            this.uiTrajanjeZahvata.Name = "uiTrajanjeZahvata";
+            this.uiTrajanjeZahvata.Size = new System.Drawing.Size(92, 13);
+            this.uiTrajanjeZahvata.TabIndex = 6;
+            this.uiTrajanjeZahvata.Text = "Trajanje zahvata: ";
+            // 
+            // uiNapomena
+            // 
+            this.uiNapomena.AutoSize = true;
+            this.uiNapomena.Location = new System.Drawing.Point(36, 336);
+            this.uiNapomena.Name = "uiNapomena";
+            this.uiNapomena.Size = new System.Drawing.Size(65, 13);
+            this.uiNapomena.TabIndex = 7;
+            this.uiNapomena.Text = "Napomena: ";
+            // 
+            // uiVrstaZahvataUnos
+            // 
+            this.uiVrstaZahvataUnos.Location = new System.Drawing.Point(113, 268);
+            this.uiVrstaZahvataUnos.Name = "uiVrstaZahvataUnos";
+            this.uiVrstaZahvataUnos.Size = new System.Drawing.Size(100, 20);
+            this.uiVrstaZahvataUnos.TabIndex = 8;
+            // 
+            // uiNapomenaUnos
+            // 
+            this.uiNapomenaUnos.Location = new System.Drawing.Point(105, 335);
+            this.uiNapomenaUnos.Multiline = true;
+            this.uiNapomenaUnos.Name = "uiNapomenaUnos";
+            this.uiNapomenaUnos.Size = new System.Drawing.Size(230, 57);
+            this.uiNapomenaUnos.TabIndex = 9;
+            // 
+            // uiTrajanjeZahvataUnos
+            // 
+            this.uiTrajanjeZahvataUnos.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.uiTrajanjeZahvataUnos.Location = new System.Drawing.Point(124, 298);
+            this.uiTrajanjeZahvataUnos.Name = "uiTrajanjeZahvataUnos";
+            this.uiTrajanjeZahvataUnos.ShowUpDown = true;
+            this.uiTrajanjeZahvataUnos.Size = new System.Drawing.Size(89, 20);
+            this.uiTrajanjeZahvataUnos.TabIndex = 10;
+            // 
+            // uiActionDodajOperaciju
+            // 
+            this.uiActionDodajOperaciju.Location = new System.Drawing.Point(169, 462);
+            this.uiActionDodajOperaciju.Name = "uiActionDodajOperaciju";
+            this.uiActionDodajOperaciju.Size = new System.Drawing.Size(105, 23);
+            this.uiActionDodajOperaciju.TabIndex = 11;
+            this.uiActionDodajOperaciju.Text = "Dodaj operaciju";
+            this.uiActionDodajOperaciju.UseVisualStyleBackColor = true;
+            // 
+            // uiActionOdustani
+            // 
+            this.uiActionOdustani.Location = new System.Drawing.Point(310, 462);
+            this.uiActionOdustani.Name = "uiActionOdustani";
+            this.uiActionOdustani.Size = new System.Drawing.Size(75, 23);
+            this.uiActionOdustani.TabIndex = 12;
+            this.uiActionOdustani.Text = "Odustani";
+            this.uiActionOdustani.UseVisualStyleBackColor = true;
             // 
             // frmDodajOperaciju
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(490, 511);
+            this.Controls.Add(this.uiActionOdustani);
+            this.Controls.Add(this.uiActionDodajOperaciju);
+            this.Controls.Add(this.uiTrajanjeZahvataUnos);
+            this.Controls.Add(this.uiNapomenaUnos);
+            this.Controls.Add(this.uiVrstaZahvataUnos);
+            this.Controls.Add(this.uiNapomena);
+            this.Controls.Add(this.uiTrajanjeZahvata);
+            this.Controls.Add(this.uiVrstaZahvata);
+            this.Controls.Add(this.uiDatumZahvataTekst);
+            this.Controls.Add(this.uiDatumZahvata);
+            this.Controls.Add(this.uiPodaciOZivotinji);
             this.Name = "frmDodajOperaciju";
             this.Text = "frmDodajOperaciju";
+            this.uiPodaciOZivotinji.ResumeLayout(false);
+            this.uiPodaciOZivotinji.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox uiPodaciOZivotinji;
+        private System.Windows.Forms.Label uiDatumRodenjaZivotinje;
+        private System.Windows.Forms.Label uiVrstaZivotinje;
+        private System.Windows.Forms.Label uiImeZivotinje;
+        private System.Windows.Forms.Label uiVlasnikZivotinje;
+        private System.Windows.Forms.Label uiDatumZahvata;
+        private System.Windows.Forms.Label uiDatumZahvataTekst;
+        private System.Windows.Forms.Label uiVrstaZahvata;
+        private System.Windows.Forms.Label uiTrajanjeZahvata;
+        private System.Windows.Forms.Label uiNapomena;
+        private System.Windows.Forms.TextBox uiVrstaZahvataUnos;
+        private System.Windows.Forms.TextBox uiNapomenaUnos;
+        private System.Windows.Forms.DateTimePicker uiTrajanjeZahvataUnos;
+        private System.Windows.Forms.Button uiActionDodajOperaciju;
+        private System.Windows.Forms.Button uiActionOdustani;
     }
 }
