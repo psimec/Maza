7c4088c992fc5e13f73747eb9e3cdfbc9ca1f57a
Paskal Šimec <psimec@foi.hr>
2018-06-23 20:47:15 +0200

    Dodaj opis funkcionalnosti klase PrijavljeniVeterinar
    Dodan opis funkcionalnosti klase PrijavljeniVeterinar

--- PI_t18024_Maza/PI_t18024_Maza/PrijavljeniVeterinar.cs
+++ PI_t18024_Maza/PI_t18024_Maza/PrijavljeniVeterinar.cs
@@ -8,11 +8,24 @@ namespace PI_t18024_Maza
 {
     public static class PrijavljeniVeterinar
     {
+        #region Varijable
+
         public static Veterinar Veterinar { get; set; }
+
+        #endregion
+
+        #region Metode
+
+        /// <summary>
+        /// Pamcenje trenutacno prijavljenog veterinara
+        /// </summary>
+        /// <param name="veterinar"> Trenutno prijavljeni veterinar </param>
 
         public static void PrijaviVeterinara(Veterinar veterinar)
         {
             Veterinar = veterinar;
         }
+
+        #endregion
     }
 }
